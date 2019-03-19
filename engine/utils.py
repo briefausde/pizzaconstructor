1f098452feb347edacb07d6fc0d9da443b4389b7
briefausde <sirkiza@gmail.com>
2019-03-19 15:26:53 +0200

    fx

--- engine/utils.py
+++ engine/utils.py
@@ -3,5 +3,5 @@ from pizzamaker.settings import SECRET_KEY
 
 
 def generate_confirm_code(order):
-    key = "{}{}{}".format(SECRET_KEY, order.created_date, order.email)
+    key = "{}{}{}".format(SECRET_KEY, order.id, order.email)
     return hashlib.sha256(key.encode()).hexdigest()
